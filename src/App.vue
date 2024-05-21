2e1c5b29e7ed7778f6eeb50446b118c83b06af02
Aleksey Yuriev <alex11-90@yandex.by>
2024-05-21 22:21:33 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="$router.push('/')">HomePage</button>
+    <button @click="$router.push('/about')">MainPage</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Start the project!!!</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
