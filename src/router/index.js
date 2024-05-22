f7b445df83ede92068e116b147e655ff8393e023
Aleksey Yuriev <alex11-90@yandex.by>
2024-05-22 15:59:35 +0300

    back to initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TaskList from '../components/TaskList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'tasks',
-      component: TaskList
-    },
     {
       path: '/about',
       name: 'about',
